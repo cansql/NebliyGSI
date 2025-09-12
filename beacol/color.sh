70e4784181e57f1ae14c509bd40d394d4a071c5b
ElewonOS <canserkan888@gmail.com>
2025-09-12 12:56:07 +0300

    Create color.sh

---
+++ beacol/color.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+RED='\e[91m'
+GREEN='\e[38;5;154m'
+YELLOW='\e[33m'
+BLUE='\033[0;34m'
+GRAY='\e[90m'
+LAVENDER_GRAY="\e[38;5;146m"
+PURPLE="\e[38;5;129m"
+CYAN='\033[0;36m'
+RESET='\033[0m'
